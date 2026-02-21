c2ce469c2d02f6498bf76884a632c091078d9dfe
Prim5v <emax27348@gmail.com>
2026-02-21 23:15:46 +0300

    4th push for test

--- backend/routes/admin/__init__.py
+++ backend/routes/admin/__init__.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 admin = Blueprint("admin", __name__, url_prefix="/admin")
 
 # Import the route modules so they are registered with the blueprint
-from . import get_all_users_from_database, get_listings, get_unverified_users, set_status, get_dashboard_overview, get_recent_system_activity, get_properties, check_transaction
+from . import get_all_users_from_database, get_listings, get_unverified_users, set_status, get_dashboard_overview, get_recent_system_activity, get_properties, check_transaction, get_plans, delete_plan, edit_plan, create_plan
 
 
