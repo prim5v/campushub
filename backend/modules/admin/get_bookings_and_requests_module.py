71689938de9960e8ca535d90d9814823b72c56ab
Prim5v <emax27348@gmail.com>
2026-02-23 13:17:47 +0300

    hh

--- backend/modules/admin/get_bookings_and_requests_module.py
+++ backend/modules/admin/get_bookings_and_requests_module.py
@@ -192,7 +192,8 @@ def get_requests_and_bookings():
                 ON p.user_id = landlord.user_id
 
             LEFT JOIN Location_data loc 
-                ON l.listing_id = loc.listing_id
+                ON p.property_id = loc.property_id
+                        
 
             LEFT JOIN (
                 SELECT s1.*
