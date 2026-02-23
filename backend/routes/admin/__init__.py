d50e00c78de1dfec246a5631b2929dfeb1677522
Prim5v <emax27348@gmail.com>
2026-02-23 17:27:57 +0300

    hh

--- backend/routes/admin/__init__.py
+++ backend/routes/admin/__init__.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 admin = Blueprint("admin", __name__, url_prefix="/admin")
 
 # Import the route modules so they are registered with the blueprint
-from . import get_all_users_from_database, get_listings, get_unverified_users, set_status, get_dashboard_overview, get_recent_system_activity, get_properties, check_transaction, get_plans, delete_plan, edit_plan, create_plan, delete_listing, fetch_listings, get_bookings_and_requests, book, pay
+from . import get_all_users_from_database, get_listings, get_unverified_users, set_status, get_dashboard_overview, get_recent_system_activity, get_properties, check_transaction, get_plans, delete_plan, edit_plan, create_plan, delete_listing, fetch_listings, get_bookings_and_requests, book, pay, set_maintenance
 
 
--- backend/routes/comrade/__init__.py
+++ backend/routes/comrade/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 comrade = Blueprint("comrade", __name__, url_prefix="/comrade")
 
-from . import get_e_service, get_listings, get_listing_details, request
+from . import get_e_service, get_listings, get_listing_details, request, maintenace_status
